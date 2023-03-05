14e21cc81433295dfdd965ee061dab615f07e5ab
Cristiano Rodrigues <crodrigues@Cristianos-MacBook-Pro.local>
2023-03-05 19:26:54 -0300

    Resolvido warning em Cliente

--- MemoryLeak/Controllers/Cliente.cs
+++ MemoryLeak/Controllers/Cliente.cs
@@ -11,8 +11,8 @@ public class Cliente: IEqualityComparer<Cliente>
         Nome = nome;
     }
 
-    public bool Equals(Cliente x, Cliente y)
-            => (x!.Id == y!.Id);
+    public bool Equals(Cliente? x, Cliente? y)
+            => (x?.Id == y?.Id);
 
         public int GetHashCode(Cliente cliente) =>
             cliente?.Id.GetHashCode() ?? throw new ArgumentNullException(nameof(cliente));
